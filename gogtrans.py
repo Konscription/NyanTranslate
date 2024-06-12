023a9197a922fd3f2f4df61b7fd0bc9f2b82460a
konviction <konviction@gmail.com>
2024-06-11 21:41:22 -0400

    translated comments in gogtrans.py

--- gogtrans.py
+++ gogtrans.py
@@ -4,19 +4,19 @@ import re
 def translate_japanese(text_dict,bahasa,asal):
     translator = Translator()
     
-     # Menghapus karakter "[" atau "]" dari setiap nilai dalam kamus
+     # Removes "[" or "]" characters from each value in the dictionary
     cleaned_text_dict2 = {key: value.replace('［', '').replace('[', '').replace(']', '').replace('「', '').replace('」', '').replace('「', '') for key, value in text_dict.items()}
     cleaned_text_dict = {key: value if value else '・' for key, value in cleaned_text_dict2.items()}
     
     print(cleaned_text_dict)
     
-    # Pisahkan teks menjadi kalimat-kalimat
+    # Split the text into sentences
     sentences = [value for key, value in cleaned_text_dict.items()]
 
-    # Terjemahkan setiap kalimat
+    # Translate each sentence
     translated_sentences = [translator.translate(sentence, src=asal, dest=bahasa).text for sentence in sentences]
 
-    # Gabungkan kembali hasil terjemahan ke dalam bentuk awal
+    # Combine the translation results back into the initial form
     translated_text_dict = {key: translated_sentences[i] for i, key in enumerate(cleaned_text_dict.keys())}
     
     hasil_terjemah = {key: value.replace("\'", '’') for key, value in translated_text_dict.items()}
